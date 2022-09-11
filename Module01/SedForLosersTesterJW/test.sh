4ed8ff1c2eec9a06f571047c6701ae1a89e2fa01
jsjohn1951 <jsjohn1951@users.noreply.github.com>
2022-09-11 15:59:15 +0400

    added tester for SedForLosers

---
+++ Module01/SedForLosersTesterJW/test.sh
@@ -0,0 +1,107 @@
+#! /bin/bash
+
+#
+#	tester created by non other than the world conqueror himself
+#	the myth, the legend.... drumroll... Willem Smith, A.k.a John
+#	A.k.a, A.k.a with indentations... Will
+#
+#	Enjoy the simple tester and conquer this project
+#
+
+printf "\nIs valgrind installed? (y/n) : "
+read varinput
+
+mkdir -p log
+printf "\n\tSed is for losers test\n\n"
+if [ -e ../ex04/sedForLosers ]
+then
+	printf "\x1B[32m\texecutable exists\n\trunning...\x1B[0m\n\n"
+else
+	printf "\tmaking executable...\x1B[0m\n"
+	make re -C ../ex04/ > log/program.log
+	printf "\x1B[32m\trunning...\x1B[0m\n\n"
+fi
+
+#
+#	run tests after executable has been made or found to exist
+#
+
+# test 1:
+sleep 1
+../ex04/sedForLosers test1 hello hi >> log/program.log
+sleep 1
+printf "\x1B[35mtest\x1B[0m ->"
+if [ -e test1.replace ]
+then
+	myVar="$(cat test1 | grep "hello" -o | wc -l )"
+	myVar2="$(cat test1.replace | grep "hi" -o | wc -l )"
+	myVar3="$(cat test1 | wc -w)"
+	myVar4="$(cat test1.replace | wc -w)"
+	if [ "$myVar" = "$myVar2" ] && [ "$myVar3" = "$myVar4" ]
+	then
+		printf " [\x1B[32mok\x1B[0m]"
+	else
+		printf " [\x1B[31mfail\x1B[0m]"
+	fi
+else
+	printf " [\x1B[31mfail\x1B[0m]"
+fi
+
+#
+#	Second test cuz we like to be extreme ;)
+#
+
+# test 2:
+sleep 1
+../ex04/sedForLosers test2 bob Jackdaniels >> log/program.log
+sleep 1
+
+if [ -e test2.replace ]
+then
+	myVar="$(cat test2 | grep "bob" -o | wc -l )"
+	myVar2="$(cat test2.replace | grep "Jackdaniels" -o | wc -l )"
+	myVar3="$(cat test2 | wc -w)"
+	myVar4="$(cat test2.replace | wc -w)"
+	if [ "$myVar" = "$myVar2" ] && [ "$myVar3" = "$myVar4" ]
+	then
+		printf " [\x1B[32mok\x1B[0m]"
+	else
+		printf " [\x1B[31mfail\x1B[0m]"
+	fi
+else
+	printf " [\x1B[31mfail\x1B[0m]"
+fi
+rm -f test2.replace
+
+# test 3:
+if [ "$varinput" == "y" ]
+then
+	sleep 1
+	valgrind --leak-check=full ../ex04/sedForLosers test2 bob Jackdaniels >> log/program.log
+	sleep 1
+	if [ -e test2.replace ]
+	then
+		myVar="$(cat test2 | grep "bob" -o | wc -l )"
+		myVar2="$(cat test2.replace | grep "Jackdaniels" -o | wc -l )"
+		myVar3="$(cat test2 | wc -w)"
+		myVar4="$(cat test2.replace | wc -w)"
+		if [ "$myVar" = "$myVar2" ] && [ "$myVar3" = "$myVar4" ]
+		then
+			printf " [\x1B[32mok\x1B[0m]"
+		else
+			printf " [\x1B[31mfail\x1B[0m]"
+			fi
+	else
+		printf " [\x1B[31mfail\x1B[0m]"
+	fi
+else
+	sleep 2
+	printf " [\x1B[31mimpossible to check for memleaks\x1B[0m]"
+	sleep 2
+fi
+
+printf "\n\n"
+
+rm -f test1.replace
+rm -f test2.replace
+make fclean -C ../ex04 >> log/program.log
---
+++ Module01/ex04/main.cpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wismith <wismith@42ABUDHABI.AE>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/09 15:58:19 by wismith           #+#    #+#             */
+/*   Updated: 2022/09/09 20:36:10 by wismith          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+void	header(std::string file, std::string s1, std::string s2)
+{
+	std::cout << "\n\t*file name:\t'\x1B[32m" << file << "\x1B[0m'" << std::endl;
+	std::cout << "\tstring s1:\t'\x1B[31m" << s1 << "\x1B[0m'" << std::endl;
+	std::cout << "\tstring s2:\t'\x1B[35m" << s2 << "\x1B[0m'" << std::endl;
+	std::cout << "\n\t\x1B[32mgenerating new file...\x1B[0m" << std::endl << std::endl;
+}
+
+void	creation(std::string file, std::string s1, std::string s2)
+{
+	std::string		save_name;
+	std::ifstream	file1;
+	std::ofstream	file2;
+	size_t			pos;
+
+	save_name = file;
+	file1.open(file);
+	if (file1)
+	{
+		file2.open(file.append(".replace"));
+		for (std::string line; getline(file1, line); )
+		{
+			while ((pos = line.find(s1)) != line.npos)
+			{
+				line.erase(pos, s1.length());
+				line.insert(pos, s2);
+			}
+			file2 << line << std::endl;
+		}
+		file2.close();
+	}
+	else
+		std::cout << save_name << ": " << "No such file or directory!\n" << std::endl;
+	file1.close();
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 4)
+		return (1);
+	header(argv[1], argv[2], argv[3]);
+	creation(argv[1], argv[2], argv[3]);
+	return (0);
+}
