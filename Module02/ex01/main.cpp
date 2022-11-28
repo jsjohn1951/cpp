984b2db5c8a8e91b0eb9f466318f32727d2937a2
Willem Smith <wismith@lab2r1s13.42abudhabi.ae>
2022-11-28 15:38:48 +0400

    changes to Makefiles

--- Module02/ex01/main.cpp
+++ Module02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: wismith <wismith@42ABUDHABI.AE>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 17:53:00 by wismith           #+#    #+#             */
-/*   Updated: 2022/11/28 15:06:53 by wismith          ###   ########.fr       */
+/*   Updated: 2022/11/28 15:33:59 by wismith          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,26 @@
 
 int main( void )
 {
-	Fixed a;
-	Fixed const b( 10 );
-	Fixed const c( 42.42f );
-	Fixed const d( b );
-	a = Fixed( 1234.4321f );
+	std::cout << std::endl;
+	{
+		Fixed a;
+		Fixed const b( 10 );
+		Fixed const c( 42.42f );
+		Fixed const d( b );
+		a = Fixed( 1234.4321f );
 
-	std::cout << "\x1B[31m" << std::endl;
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
-	std::cout << "\x1B[32m" << std::endl;
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-	std::cout << "\x1B[0m" << std::endl;
+		std::cout << "\x1B[31m" << std::endl;
+		std::cout << "a is " << a << std::endl;
+		std::cout << "b is " << b << std::endl;
+		std::cout << "c is " << c << std::endl;
+		std::cout << "d is " << d << std::endl;
+		std::cout << "\x1B[32m" << std::endl;
+		std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+		std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+		std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+		std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+		std::cout << "\x1B[0m" << std::endl;
+	}
+	std::cout << std::endl;
 	return 0;
 }
