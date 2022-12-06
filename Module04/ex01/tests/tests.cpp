c342bd504c11e2854d0101954e5c841175197cb2
Willem Smith <wismith@lab1r2s6.42abudhabi.ae>
2022-12-06 16:15:41 +0400

    tests added

--- Module04/ex01/tests/tests.cpp
+++ Module04/ex01/tests/tests.cpp
@@ -6,7 +6,7 @@
 /*   By: wismith <wismith@42ABUDHABI.AE>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 19:14:18 by wismith           #+#    #+#             */
-/*   Updated: 2022/12/05 22:32:51 by wismith          ###   ########.fr       */
+/*   Updated: 2022/12/06 16:14:26 by wismith          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,35 +82,57 @@ void	Wrong_Animal_t()
 void	Brain_test()
 {
 	std::cout << std::endl;
-	//{
-	// 	Cat *cat = new Cat;
-	// 	Dog *dog = new Dog;
+	std::cout << "Animal Has Brain Test Default : \x1B[32m" << std::endl << std::endl;
+	{
+		int	num = 4;
+		const Animal	*j[num];
 
-	// 	std::cout << std::endl;
-	// 	Cat	cat2(*cat);
-	// 	Dog dog2(*dog);
-	// 	std::cout << std::endl;
-
-	// 	std::cout << std::endl;
-	// 	cat2 = *cat;
-	// 	dog2 = *dog;
-	// 	std::cout << std::endl;
-
-	// 	std::cout << std::endl;
-	// 	delete cat;
-	// 	delete dog;
-	// 	std::cout << std::endl;
-	// }
+		for (int i = 0; i < num; i++)
+		{
+			(i % 2 ? j[i] = new Dog : j[i] = new Cat);
+			std::cout << std::endl;
+		}
+		std::cout << "---------------------------" << std::endl << std::endl;
+		for (int i = 0; i < num; i++)
+		{
+			delete j[i];
+			std::cout << std::endl;
+		}
+	}
+	std::cout << std::endl << "\x1B[0mAnimal Has Brain Test One : \x1B[36m" << std::endl << std::endl;
 	{
 		const Animal* j = new Dog();
 		std::cout << std::endl;
 		const Animal* i = new Cat();
-
 		std::cout << std::endl;
+
+		std::cout << "---------------------------" << std::endl << std::endl;
 
 		delete j;//should not create a leak
 		std::cout << std::endl;
 		delete i;
 	}
-	std::cout << std::endl;
+	std::cout << std::endl << "\x1B[0mAnimal Has Brain Test Two :" << std::endl << std::endl;
+	std::cout << "\t Testing the copy constructors and\n\tcopy assignment operator overload \x1B[31m" << std::endl
+	<< std::endl;
+	{
+		Cat *cat = new Cat;
+		Dog *dog = new Dog;
+
+		std::cout << std::endl;
+		Cat	cat2(*cat);
+		Dog dog2(*dog);
+		std::cout << std::endl;
+
+		std::cout << std::endl;
+		cat2 = *cat;
+		dog2 = *dog;
+		std::cout << std::endl;
+
+		std::cout << std::endl;
+		delete cat;
+		delete dog;
+		std::cout << std::endl;
+	}
+	std::cout << "\x1B[0m" << std::endl;
 }
