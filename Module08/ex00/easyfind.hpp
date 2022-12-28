eff416c626602d53fc7df65ac23e42d91daa240c
Willem Smith <wismith@lab1r1s6.42abudhabi.ae>
2022-12-28 22:10:05 +0400

    adding Module08

---
+++ Module08/ex00/easyfind.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wismith <wismith@42ABUDHABI.AE>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/28 20:03:43 by wismith           #+#    #+#             */
+/*   Updated: 2022/12/28 20:09:03 by wismith          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#	ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+# include <algorithm>
+
+template <typename T>
+void	easyfind(const T &t, int i)
+{
+
+}
+
+#endif
